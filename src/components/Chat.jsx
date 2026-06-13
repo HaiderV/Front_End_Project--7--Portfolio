4954ced888f33d8b79dd44409c7148d4faf6178a
HaiderV <haidervadgamwala3@gmail.com>
2026-06-13 13:05:37 +0530

    Backend Connected

--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -61,7 +61,7 @@ const Chat = ({ onClose }) => {
         setIsTyping(true);
 
         try {
-            const response = await fetch("http://localhost:5000/chat", {
+            const response = await fetch(`${import.meta.env.VITE_API_URL}/chat`, {
                 method: "POST",
                 headers: {
                     "Content-Type": "application/json",
